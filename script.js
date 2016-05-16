307a1dcddcbe68fd514e34e40ccbf119bd8e9036
Chris Barry <chris_barry@yahoo.com>
2016-05-16 13:52:38 -0700

    ngeventtest1

--- script.js
+++ script.js
@@ -38,3 +38,50 @@ scotchApp.controller('aboutController', function($scope) {
 scotchApp.controller('contactController', function($scope) {
     $scope.message = 'Contact us! JK. This is just a demo.';
 });
+
+scotchApp.controller('authController', function($scope) {
+    $scope.authenticate = function() {
+      $scope.message = 'Called authController!';
+      authClient.signIn({
+        username: 'chris.barry',
+        password: 'Training123'
+      })
+      .then(function(transaction) { // On success
+        switch(transaction.status) {
+
+          case 'SUCCESS':
+            authClient.session.setCookieAndRedirect(transaction.sessionToken); // Sets a cookie on redirect
+            break;
+
+          default:
+            throw 'We cannot handle the ' + transaction.status + ' status';
+        }
+      })
+      .fail(function(err) { // On failure
+        console.error(err);
+      });
+    }
+});
+
+// Okta AuthN API
+var authClient = new OktaAuth({url: 'https://cbarry.okta.com'});
+/*
+authClient.signIn({
+  username: 'some-username',
+  password: 'some-password'
+})
+.then(function(transaction) { // On success
+  switch(transaction.status) {
+
+    case 'SUCCESS':
+      authClient.session.setCookieAndRedirect(transaction.sessionToken); // Sets a cookie on redirect
+      break;
+
+    default:
+      throw 'We cannot handle the ' + transaction.status + ' status';
+  }
+})
+.fail(function(err) { // On failure
+  console.error(err);
+});
+*/
